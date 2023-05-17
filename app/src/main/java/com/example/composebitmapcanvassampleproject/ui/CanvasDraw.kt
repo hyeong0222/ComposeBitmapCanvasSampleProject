7035b79b52fc36947e87c4b9f253e5110323a44e
Sang Hyeong Woo <sang.woo@weversecompany.com>
2023-05-17 17:32:26 +0900

    Composable Box Layout

--- app/src/main/java/com/example/composebitmapcanvassampleproject/ui/CanvasDraw.kt
+++ app/src/main/java/com/example/composebitmapcanvassampleproject/ui/CanvasDraw.kt
@@ -1,69 +1,69 @@
 package com.example.composebitmapcanvassampleproject.ui
 
-import android.content.Context
+import android.graphics.Bitmap
 import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
-import androidx.core.graphics.drawable.toBitmap
-import coil.ImageLoader
-import coil.compose.AsyncImagePainter
-import coil.compose.rememberAsyncImagePainter
-import coil.request.ImageRequest
-import coil.request.SuccessResult
-import com.example.composebitmapcanvassampleproject.model.Sample
-import kotlinx.coroutines.launch
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.composebitmapcanvassampleproject.viewmodel.MainViewModel
 
 @Composable
-fun CanvasDraw(sampleList: List<Sample>) {
+fun CanvasDraw(
+    viewModel: MainViewModel = hiltViewModel()
+) {
     val context = LocalContext.current
     val configuration = LocalConfiguration.current
+    val density = LocalDensity.current
 
     Log.e("", "++++++++++++++++screen height = ${configuration.screenHeightDp}")
     Log.e("", "++++++++++++++++screen width = ${configuration.screenWidthDp}")
 
-    // 디바이스 사이즈 이용해서 16 x 9 비율로 width, height 정하기
-    val height = configuration.screenWidthDp - 50
-    val width = ((height * 16) / 9)
+    // 디바이스 사이즈 이용해서 16 x 9 비율로 width, height 정하기 (dp)
+    val canvasHeight = configuration.screenWidthDp - 50
+    val canvasWidth = ((canvasHeight * 16) / 9)
 
-    val bitmapList = mutableListOf<ImageBitmap>()
-    LaunchedEffect(true) {
-        bitmapList.addAll(createBitmapList(context = context, sampleList = sampleList))
-    }
+    val bitmapList by viewModel.bitmapList.collectAsState()
 
     Canvas(
         modifier = Modifier
-            .size(width = width.dp, height = height.dp)
+            .size(width = canvasWidth.dp, height = canvasHeight.dp)
             .background(Color.Red)
     ) {
-        bitmapList.forEach { bitmap ->
-            drawImage(image = bitmap)
+//        val image = ImageBitmap(500, 250).
+//        val bitmap = Bitmap.createBitmap(500, 250, Bitmap.Config.ARGB_8888)
+//        val option = BitmapFactory.Options()
+//        option.inPreferredConfig = Bitmap.Config.ARGB_8888
+//        val bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.youtube, option).asImageBitmap()
+//
+//        drawImage(bitmap, topLeft = Offset(500f, 300f))
+
+        if (bitmapList.isNotEmpty()) {
+            bitmapList.forEachIndexed { index, bitmap ->
+                val sample = viewModel.sampleList[index]
+                with(density) {
+                    val bitmapWidth = (sample.scaleX * canvasWidth).dp.roundToPx()
+                    val bitmapHeight = (sample.scaleY * canvasHeight).dp.roundToPx()
+                    val resizedBitmap = Bitmap.createScaledBitmap(
+                        bitmap, bitmapWidth, bitmapHeight,false
+                    ).asImageBitmap()
+
+                    val offsetX = canvasWidth.dp.roundToPx() * sample.positionX
+                    val offsetY = canvasHeight.dp.roundToPx() * sample.positionY
+                    drawImage(image = resizedBitmap, topLeft = Offset(offsetX, offsetY))
+                }
+            }
         }
     }
-}
-
-suspend fun createBitmapList(context: Context, sampleList: List<Sample>): List<ImageBitmap> {
-    val bitmapList = mutableListOf<ImageBitmap>()
-
-    sampleList.forEach { sample ->
-        val loader = ImageLoader(context)
-        val request = ImageRequest.Builder(context).data(sample.value).build()
-
-        val bitmap = (loader.execute(request) as SuccessResult).drawable.toBitmap()
-        bitmapList.add(bitmap.asImageBitmap())
-    }
-
-    return bitmapList
 }
---
+++ app/src/main/java/com/example/composebitmapcanvassampleproject/viewmodel/MainViewModekl.kt
@@ -0,0 +1,131 @@
+package com.example.composebitmapcanvassampleproject.viewmodel
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Rect
+import android.graphics.drawable.BitmapDrawable
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.core.graphics.drawable.toBitmap
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.ImageLoader
+import coil.request.ImageRequest
+import coil.request.SuccessResult
+import com.example.composebitmapcanvassampleproject.model.Sample
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    @ApplicationContext private val context: Context
+) : ViewModel() {
+
+    val sampleList: List<Sample>
+        get() = listOf(
+            Sample(
+                type = "IMAGE",
+                value = "https://avatars3.githubusercontent.com/u/35650605?s=400&u=058086fd5c263f50f2fbe98ed24b5fbb7d437a4e&v=4",
+                scaleX = 0.2f,
+                scaleY = 0.2f,
+                positionX = 0.24f,
+                positionY = 0.3f,
+                sort = 0
+            ),
+            Sample(
+                type = "IMAGE",
+                value = "https://avatars3.githubusercontent.com/u/35650605?s=400&u=058086fd5c263f50f2fbe98ed24b5fbb7d437a4e&v=4",
+                scaleX = 0.3f,
+                scaleY = 0.3f,
+                positionX = 0.6f,
+                positionY = 0.2f,
+                sort = 0
+            ),
+        )
+
+    private val _bitmapList = MutableStateFlow(listOf<Bitmap>())
+    val bitmapList = _bitmapList.asStateFlow()
+
+    init {
+        createBitmapList(sampleList = sampleList)
+    }
+
+    private fun createBitmapList(sampleList: List<Sample>) {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                val bitmapList = mutableListOf<Bitmap>()
+
+                val loader = ImageLoader(context)
+
+                sampleList.forEach { sample ->
+                    val request = ImageRequest.Builder(context = context)
+                        .data(data = sample.value)
+                        .allowHardware(enable = false)
+                        .build()
+
+                    val result = loader.execute(request)
+                    if (result is SuccessResult) {
+                        bitmapList.add(result.drawable.toBitmap())
+                    }
+                }
+
+                _bitmapList.value = bitmapList
+            }
+        }
+    }
+
+    fun drawCanvas() {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                val loader = ImageLoader(context)
+                val request = ImageRequest.Builder(context)
+                    .data("https://avatars3.githubusercontent.com/u/35650605?s=400&u=058086fd5c263f50f2fbe98ed24b5fbb7d437a4e&v=4")
+                    .allowHardware(false) // Disable hardware bitmaps.
+                    .build()
+
+//            val portraitBitmap = Bitmap.createBitmap(1080, 1920, Bitmap.Config.ARGB_8888)
+//            val portraitCanvas = Canvas(portraitBitmap)
+//            portraitCanvas.drawColor(Color.WHITE)
+//
+                val landscapeBitmap = Bitmap.createBitmap(1920, 1080, Bitmap.Config.ARGB_8888)
+                val landscapeCanvas = Canvas(landscapeBitmap)
+//            landscapeCanvas.drawColor(Color.WHITE)
+                val result = loader.execute(request)
+                if (result is SuccessResult) {
+                    val profile = (result.drawable as BitmapDrawable).bitmap
+                    val portraitOffsetY = 238
+                    val landscapeOffsetY = 88
+                    val imageSize = 478
+//                    portraitCanvas.drawBitmap(
+//                        profile,
+//                        null,
+//                        Rect(
+//                            (1080 - imageSize) / 2,
+//                            (1920 - imageSize) / 2 - portraitOffsetY,
+//                            (1080 - imageSize) / 2 + imageSize,
+//                            (1920 - imageSize) / 2 - portraitOffsetY + imageSize
+//                        ),
+//                        null
+//                    )
+                    landscapeCanvas.drawBitmap(
+                        profile,
+                        null,
+                        Rect(
+                            (1920 - imageSize) / 2,
+                            (1080 - imageSize) / 2 - landscapeOffsetY,
+                            (1920 - imageSize) / 2 + imageSize,
+                            (1080 - imageSize) / 2 - landscapeOffsetY + imageSize
+                        ),
+                        null
+                    )
+                }
+            }
+        }
+    }
+}
